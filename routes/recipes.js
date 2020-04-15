aef8e90baf3957b381cca617cfd314d382cd9830
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-04-14 20:20:59 -0500

    Removing debug statements.

--- routes/recipes.js
+++ routes/recipes.js
@@ -20,20 +20,28 @@ insecureRouter.get('/', async function (_, res) {
 });
 
 async function processRecipe(recipe) {
+    console.log('Processing recipe ' + recipe.name);
     recipe.steps = JSON.parse(recipe.steps)
     const usages = await cosmos.getConnectedEntriesOfKind(recipe.id, 'ingredientUsage', [])
     const usagesPromises = usages.map(processIngredientUsages)
     recipe.ingredients = await Promise.all(usagesPromises);
+    console.log('Finished processing recipe ' + recipe.name);
     return recipe;
 }
 
 async function processIngredientUsages(usage) {
+    console.log('Processing ingredient usage ' + usage.id)
     const ingred = {}
+    console.log('Processing units for ' + usage.id)
     const unit = (await cosmos.getConnectedEntriesOfKind(usage.id, 'unit', ['name'], ['unitAmount']))[0]
+    console.log('Finished processing units for ' + usage.id)
     ingred.unit = unit.vertex.name;
     ingred.amount = unit.edge.unitAmount;
+    console.log('Processing ingredient for ' + usage.id)
     const ingredient = (await cosmos.getConnectedEntriesOfKind(usage.id, 'ingredient', ['name']))[0]
+    console.log('Finished processing ingredient for ' + usage.id)
     ingred.ingredient = ingredient.vertex.name;
+    console.log('Finished processing ingredient usage for ' + usage.id)
     return ingred;
 }
 
