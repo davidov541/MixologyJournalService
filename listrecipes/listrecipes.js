9a3b30594c6eb5e64b2cccb4c5ef3d85665a1374
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-09 19:50:25 -0500

    Removing requirement that we authenticate to list out recipes. We still want to try to authenticate to get user information if possible.

--- listrecipes/listrecipes.js
+++ listrecipes/listrecipes.js
@@ -7,31 +7,23 @@ module.exports = async (context, req) => {
 
     const securityResult = security.checkToken(context, req);
 
-    if (!securityResult.success)
-    {
+    try {
+        const info = await cosmos.getAllDescendentsOfKind('recipe')
+
+        var recipes = new Array();
+        for(recipe in info) {
+            recipes.push(entityConversion.processRecipe(info[recipe]));
+        }
+
         context.res = {
-            status: securityResult.error.code,
-            body: securityResult.error.message
-        }
-    } else {
-        try {
-            const info = await cosmos.getAllDescendentsOfKind('recipe')
-    
-            var recipes = new Array();
-            for(recipe in info) {
-                recipes.push(entityConversion.processRecipe(info[recipe]));
-            }
-    
-            context.res = {
-                status: 200,
-                body: recipes
-            };
-        } catch (err) {
-            console.log(err)
-            context.res = {
-                status: 500,
-                body: "Error found: " + err
-            };
-        }
+            status: 200,
+            body: recipes
+        };
+    } catch (err) {
+        console.log(err)
+        context.res = {
+            status: 500,
+            body: "Error found: " + err
+        };
     }
 };
