168d6bc84767db5a1f0c5595d3774afe5c330273
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-10 17:02:51 -0500

    Adding ability to delete drink

---
+++ deletedrink/deletedrink.js
@@ -0,0 +1,31 @@
+const cosmos = require('../util/cosmos')
+const security = require('../util/security')
+
+module.exports = async function (context, req) {
+    context.log('DELETE /secure/drinks');
+    
+    const securityResult = security.checkToken(context, req);
+
+    if (!securityResult.success)
+    {
+        context.res = {
+            status: securityResult.error.code,
+            body: securityResult.error.message
+        }
+    } else {
+        try {
+            const id = req.body.id;
+            await cosmos.deleteEntry(id, ['uses']);
+            context.res = {
+                status: 200,
+                body: "Success"
+            }
+        } catch (err) {
+            console.log(err)
+            context.res = {
+                status: 500,
+                body: "Error found: " + err
+            };
+        }
+    }
+};
