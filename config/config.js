4537ef450ef56f9f86dc9551b9955640b1f28e0f
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-09 19:08:12 -0500

    Removing config.js from the repo.

--- config/config.js
+++
@@ -1,9 +0,0 @@
-var config = {}
-
-config.endpoint = "wss://mixologyjournal.gremlin.cosmos.azure.com:443/";
-config.primaryKey = "bAjL5DaW8Vlqb21v0AqNWmNjlwSLySuP2ps76gHbNVMRJqnP07HngHo2wj1DyCLQkoBdz7TXLLzm0Q1wvbh8dA==";
-config.database = "mixologyjournal"
-config.collection = "mixologyjournal"
-config.env = "dev"
-
-module.exports = config;
