2ef42db89ec1d356c133862699df718b74de6693
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-11 20:08:45 -0500

    Adding ability to create edge as well.

--- writeToCosmos/writeToCosmos.js
+++ writeToCosmos/writeToCosmos.js
@@ -1,5 +1,13 @@
 ﻿const cosmos = require('../util/cosmos');
 
 module.exports = async function(context, msg) {
-    await cosmos.createEntryOfKind(msg.kind, msg.id, msg.properties, msg.edges);
+    switch(msg.entityType)
+    {
+        case 'vertex':
+            await cosmos.createEntryOfKind(msg.kind, msg.id, msg.properties, msg.edges);
+            break;
+        case 'edge':
+            await cosmos.createEdge(msg.source, msg.target, msg.relationship, msg.properties);
+            break;
+    }
 };
