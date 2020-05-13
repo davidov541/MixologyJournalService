a36b15ad9683900aa2f9b53b35b20a64afe4229e
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-12 22:47:17 -0500

    Adding testing capabilities to the project.

--- ping/helloworld.js
+++ ping/helloworld.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, _) {
     context.log('GET /');
     context.res = {
-        // status: 200, /* Defaults to 200 */
+        status: 200,
         body: "Hello World"
     };
 };
---
+++ test/ping.test.js
@@ -0,0 +1,20 @@
+const helloworld = require('../ping/helloworld')
+
+describe('Ping Function Tests', function () {
+    test('should return successfully at all times.', async function () {
+        var context = {   
+            res: {},
+            log: function (msg) {}        
+        }
+
+        const request = {}
+
+        await helloworld(context, request);
+
+        const expectedResponse = {
+            body: "Hello World",
+            status: 200
+        }
+        expect(context.res).toEqual(expectedResponse);
+    });
+});
