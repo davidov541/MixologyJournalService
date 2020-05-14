2cf47e86cae353c05d649447fa6ff99200846742
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-13 20:37:49 -0500

    Adding some log statements to help debug issue.

--- config/config.js
+++ config/config.js
@@ -11,18 +11,23 @@ async function getConfig() {
     const sas = process.env.ADLS_SASTOKEN;
     const fileSystemName = process.env.ADLS_CONFIGFSNAME;
     const fileName = process.env.ADLS_CONFIGFILENAME;
-    const serviceClient = new DataLakeServiceClient(
-      `https://${account}.dfs.core.windows.net/${fileSystemName}${sas}`
-    );
+    const accountURL = `https://${account}.dfs.core.windows.net/${fileSystemName}${sas}`;
+    console.log("Account URL = " + accountURL);
+    const serviceClient = new DataLakeServiceClient(accountURL);
 
+    console.log("Got client");
     const fileSystemClient = serviceClient.getFileSystemClient(
       fileSystemName
     );
+    console.log("Got file system");
     const fileClient = fileSystemClient.getFileClient(fileName);
+    console.log("Got file");
     const downloadResponse = await fileClient.read();
+    console.log("Got response");
     config.signingToken = await streamToString(
       downloadResponse.readableStreamBody
     );
+    console.log("Got signing token.")
     isInitialized = true;
   }
   return config;
