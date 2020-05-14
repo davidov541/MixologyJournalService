182da06c8489789f6382532f0ca9b45837f8e120
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-13 21:45:52 -0500

    Adding more debug statements.

--- listrecipes/listrecipes.js
+++ listrecipes/listrecipes.js
@@ -27,7 +27,7 @@ module.exports = async function (context, req) {
         console.log(err)
         context.res = {
             status: 500,
-            body: "Error found: " + err
+            body: "Error found: " + err + "\nInput: " + JSON.stringify(req)
         };
     }
 };
