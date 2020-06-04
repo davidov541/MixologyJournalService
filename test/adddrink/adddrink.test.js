731bbc44432e48655bf80fb5221b8049b6430d16
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-06-03 22:54:36 -0500

    Adding full unit test for add drink module.

--- test/adddrink/adddrink.test.js
+++ test/adddrink/adddrink.test.js
@@ -7,6 +7,10 @@ function setupMockSecurity() {
     return sinon.mock(uut.__get__("security"))
 }
 
+function setupMockPersistence() {
+    return sinon.mock(uut.__get__("cosmos"))
+}
+
 describe('Add Drink Function Tests', function () {
     test('should return an error if the authentication fails.', async function () {
         const mockSecurity = setupMockSecurity()
@@ -27,10 +31,10 @@ describe('Add Drink Function Tests', function () {
         const request = {}
 
         mockSecurity
-        .expects("checkToken")
-        .once()
-        .withArgs(context, request)
-        .returns(mockResult)
+            .expects("checkToken")
+            .once()
+            .withArgs(context, request)
+            .returns(mockResult)
 
         await uut(context, request);
 
@@ -41,5 +45,168 @@ describe('Add Drink Function Tests', function () {
         expect(context.res).toEqual(expectedResponse);
 
         mockSecurity.verify()
+    });
+
+    test('should correctly add the recipe if authentication succeeds.', async function () {
+        const mockSecurity = setupMockSecurity()
+        const mockPersistence = setupMockPersistence()
+        
+        const mockSecurityResult = {
+            "success": true,
+            "error": {
+                "code": "Test Code",
+                "message": "Test Message"
+            },
+            "user": {
+                "payload": {
+                    "sub": "User"
+                }
+            }
+        }
+
+        var context = {   
+            res: {},
+            log: function (msg) {}        
+        }
+
+        const request = {
+            "body": {
+                "name": "Test Recipe",
+                "ingredients": [
+                    {
+                        "ingredient": {
+                            "id": "Ingredient-1"
+                        },
+                        "unit": {
+                            "id": "Unit-1"
+                        },
+                        "amount": "1.0"
+                    },
+                    {
+                        "ingredient": {
+                            "id": "Ingredient-2"
+                        },
+                        "unit": {
+                            "id": "Unit-2"
+                        },
+                        "amount": "2.0"
+                    }
+                ],
+                "steps": [
+                    "Step 1",
+                    "Step 2",
+                    "Step 3"
+                ],
+                "sourceRecipeID": "Recipe-1",
+                "review": "Test Review",
+                "rating": "1.0",
+                
+            }
+        }
+
+        const expectations = [
+            mockSecurity
+                .expects("checkToken")
+                .once()
+                .withArgs(context, request)
+                .returns(mockSecurityResult),
+            mockSecurity
+                .expects("isAdmin")
+                .once()
+                .withArgs(mockSecurityResult.user)
+                .returns(false),
+            mockPersistence.expects("createEntryOfKind")
+                .once()
+                .withExactArgs('ingredientUsage', sinon.match.any, {
+                    "name": "Test Recipe Instance Ingredient Usage #1"
+                }, [
+                    {
+                        "id": "Ingredient-1",
+                        "relationship": "of",
+                        "properties": {}
+                    },
+                    {
+                        "id": "Unit-1",
+                        "relationship": "amount",
+                        "properties": {
+                            "unitAmount": "1.0"
+                        }
+                    }
+                ]),
+            
+            mockPersistence.expects("createEntryOfKind")
+                .once()
+                .withExactArgs('ingredientUsage', sinon.match.any, {
+                    "name": "Test Recipe Instance Ingredient Usage #2"
+                }, [
+                    {
+                        "id": "Ingredient-2",
+                        "relationship": "of",
+                        "properties": {}
+                    },
+                    {
+                        "id": "Unit-2",
+                        "relationship": "amount",
+                        "properties": {
+                            "unitAmount": "2.0"
+                        }
+                    }
+                ]),
+            mockPersistence.expects("createEntryOfKind")
+                .once()
+                .withExactArgs('drink', sinon.match.any, {
+                    "name": "Test Recipe",
+                    "steps": "%5B%22Step%201%22%2C%22Step%202%22%2C%22Step%203%22%5D"
+                }, [
+                    {
+                        "id": sinon.match.any,
+                        "relationship": "uses",
+                        "properties": {}
+                    },
+                    {
+                        "id": sinon.match.any,
+                        "relationship": "uses",
+                        "properties": {}
+                    }
+                ]),
+            mockPersistence.expects("createEntryOfKind")
+                .once()
+                .withExactArgs('review', sinon.match.any, {
+                    "name": "Test Recipe",
+                    "rating": "1.0",
+                    "review": "Test%20Review"
+                }, [
+                    {
+                        "id": sinon.match.any,
+                        "relationship": "reviews",
+                        "properties": {}
+                    }
+                ]),
+            mockPersistence.expects("createEdge")
+                .once()
+                .withExactArgs(sinon.match.any, sinon.match.any, 'review of', {}),
+            mockPersistence.expects("createEdge")
+                .once()
+                .withExactArgs(sinon.match.any, sinon.match.any, 'derived from', {}),
+            mockPersistence.expects("createEdge")
+                .once()
+                .withExactArgs(sinon.match.any, sinon.match.any, 'derivative', {}),
+            mockPersistence.expects("createEdge")
+                .once()
+                .withExactArgs(sinon.match.any, "User", 'created by', {}),
+            mockPersistence.expects("createEdge")
+                .once()
+                .withExactArgs("User", sinon.match.any, 'created', {})
+        ]
+            
+        await uut(context, request);
+
+        const expectedResponse = {
+            "body": "Success"
+        }
+        expect(context.res).toEqual(expectedResponse);
+
+        mockSecurity.verify()
+        expectations.map(e => e.verify())
     });
 });
