c06d6db56cb709aef53b6f41a73383156a72e3b4
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-06-04 21:50:13 -0500

    Simplifying test for list ingredients.

--- test/listingredients/listingredients.test.js
+++ test/listingredients/listingredients.test.js
@@ -11,109 +11,67 @@ function setupMockPersistence() {
     return sinon.mock(uut.__get__("cosmos"))
 }
 
+async function runSuccessfulListTest(securitySuccess) {
+    const mockSecurity = setupMockSecurity()
+    const mockPersistence = setupMockPersistence()
+
+    const mockSecurityResult = {
+        "success": securitySuccess,
+        "error": {
+            "code": "Test Code",
+            "message": "Test Message"
+        },
+        "user": {
+            "payload": {
+                "sub": "User"
+            }
+        }
+    }
+
+    var context = {   
+        res: {},
+        log: function (msg) {}        
+    }
+
+    const request = {}
+
+    const mockList = [
+        {
+            "name": "Ingredient 1"
+        },
+        {
+            "name": "Ingredient 2"
+        }
+    ]
+
+    const expectations = [
+        mockSecurity
+            .expects("checkToken")
+            .once()
+            .withArgs(context, request)
+            .returns(mockSecurityResult),
+        mockPersistence.expects("getEntriesOfKind")
+            .once()
+            .withExactArgs('ingredient', ['name'])
+            .returns(mockList),
+    ]
+        
+    await uut(context, request);
+
+    expect(context.res.body).toEqual(mockList);
+
+    expectations.map(e => e.verify())
+
+    mockSecurity.restore()
+    mockPersistence.restore()
+}
+
 describe('List Ingredients Function Tests', function () {
     test('should correctly list ingredients if authentication succeeds.', async function () {
-        const mockSecurity = setupMockSecurity()
-        const mockPersistence = setupMockPersistence()
-
-        const mockSecurityResult = {
-            "success": true,
-            "error": {
-                "code": "Test Code",
-                "message": "Test Message"
-            },
-            "user": {
-                "payload": {
-                    "sub": "User"
-                }
-            }
-        }
-
-        var context = {   
-            res: {},
-            log: function (msg) {}        
-        }
-
-        const request = {}
-
-        const mockList = [
-            {
-                "name": "Ingredient 1"
-            },
-            {
-                "name": "Ingredient 2"
-            }
-        ]
-
-        const expectations = [
-            mockSecurity
-                .expects("checkToken")
-                .once()
-                .withArgs(context, request)
-                .returns(mockSecurityResult),
-            mockPersistence.expects("getEntriesOfKind")
-                .once()
-                .withExactArgs('ingredient', ['name'])
-                .returns(mockList),
-        ]
-            
-        await uut(context, request);
-
-        expect(context.res.body).toEqual(mockList);
-
-        expectations.map(e => e.verify())
-
-        mockSecurity.restore()
-        mockPersistence.restore()
+        await runSuccessfulListTest(true)
     });
 
     test('should correctly list ingredients even if authentication fails.', async function () {
-        const mockSecurity = setupMockSecurity()
-        const mockPersistence = setupMockPersistence()
-
-        const mockSecurityResult = {
-            "success": false,
-            "error": {
-                "code": "Test Code",
-                "message": "Test Message"
-            }
-        }
-
-        var context = {   
-            res: {},
-            log: function (msg) {}        
-        }
-
-        const request = {}
-
-        const mockList = [
-            {
-                "name": "Ingredient 1"
-            },
-            {
-                "name": "Ingredient 2"
-            }
-        ]
-
-        const expectations = [
-            mockSecurity
-                .expects("checkToken")
-                .once()
-                .withArgs(context, request)
-                .returns(mockSecurityResult),
-            mockPersistence.expects("getEntriesOfKind")
-                .once()
-                .withExactArgs('ingredient', ['name'])
-                .returns(mockList),
-        ]
-            
-        await uut(context, request);
-
-        expect(context.res.body).toEqual(mockList);
-
-        expectations.map(e => e.verify())
-
-        mockSecurity.restore()
-        mockPersistence.restore()
+        await runSuccessfulListTest(false)
     });
 });
