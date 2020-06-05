78c87735e6215f2c256063fe5facff0ee50466a6
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-06-04 20:53:40 -0500

    Adding tests for adding a unit.

---
+++ test/addunit/addunit.test.js
@@ -0,0 +1,97 @@
+const rewire = require('rewire');
+const sinon = require('sinon');
+
+const uut = rewire('../../addunit/addunit')
+
+function setupMockSecurity() {
+    return sinon.mock(uut.__get__("security"))
+}
+
+function setupMockPersistence() {
+    return sinon.mock(uut.__get__("cosmos"))
+}
+
+describe('Add Unit Function Tests', function () {
+    test('should return an error if the authentication fails.', async function () {
+        const mockSecurity = setupMockSecurity()
+        
+        const mockResult = {
+            "success": false,
+            "error": {
+                "code": "Test Code",
+                "message": "Test Message"
+            }
+        }
+
+        var context = {   
+            res: {},
+            log: function (msg) {}        
+        }
+
+        const request = {}
+
+        mockSecurity
+            .expects("checkToken")
+            .once()
+            .withArgs(context, request)
+            .returns(mockResult)
+
+        await uut(context, request);
+
+        const expectedResponse = {
+            "status": mockResult.error.code,
+            "body": mockResult.error.message
+        }
+        expect(context.res).toEqual(expectedResponse);
+
+        mockSecurity.verify()
+    });
+
+    test('should correctly add the unit if authentication succeeds.', async function () {
+        const mockSecurity = setupMockSecurity()
+        const mockPersistence = setupMockPersistence()
+
+        const mockSecurityResult = {
+            "success": true,
+            "error": {
+                "code": "Test Code",
+                "message": "Test Message"
+            },
+            "user": {
+                "payload": {
+                    "sub": "User"
+                }
+            }
+        }
+
+        var context = {   
+            res: {},
+            log: function (msg) {}        
+        }
+
+        const request = {
+            "body": {
+                "name": "Test Unit"
+            }
+        }
+
+        const expectations = [
+            mockSecurity
+                .expects("checkToken")
+                .once()
+                .withArgs(context, request)
+                .returns(mockSecurityResult),
+            mockPersistence.expects("createEntryOfKind")
+                .once()
+                .withExactArgs('unit', sinon.match.any, {
+                    "name": request.body.name
+                }, []),
+        ]
+            
+        await uut(context, request);
+
+        expect(context.res.body.name).toEqual(request.body.name);
+
+        expectations.map(e => e.verify())
+    });
+});
