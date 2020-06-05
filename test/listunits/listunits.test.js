00aebca9c49d5d9679dbd9f51281695fe05de66a
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-06-04 21:52:06 -0500

    Simplifying test for list units.

--- test/listunits/listunits.test.js
+++ test/listunits/listunits.test.js
@@ -11,109 +11,62 @@ function setupMockPersistence() {
     return sinon.mock(uut.__get__("cosmos"))
 }
 
+async function runSuccessfulListTest(securitySuccess) {
+    const mockSecurity = setupMockSecurity()
+    const mockPersistence = setupMockPersistence()
+
+    const mockSecurityResult = {
+        "success": securitySuccess,
+        "error": {
+            "code": "Test Code",
+            "message": "Test Message"
+        }
+    }
+
+    var context = {   
+        res: {},
+        log: function (msg) {}        
+    }
+
+    const request = {}
+
+    const mockList = [
+        {
+            "name": "Unit 1"
+        },
+        {
+            "name": "Unit 2"
+        }
+    ]
+
+    const expectations = [
+        mockSecurity
+            .expects("checkToken")
+            .once()
+            .withArgs(context, request)
+            .returns(mockSecurityResult),
+        mockPersistence.expects("getEntriesOfKind")
+            .once()
+            .withExactArgs('unit', ['name'])
+            .returns(mockList),
+    ]
+        
+    await uut(context, request);
+
+    expect(context.res.body).toEqual(mockList);
+
+    expectations.map(e => e.verify())
+
+    mockSecurity.restore()
+    mockPersistence.restore()
+}
+
 describe('List Units Function Tests', function () {
     test('should correctly list units if authentication succeeds.', async function () {
-        const mockSecurity = setupMockSecurity()
-        const mockPersistence = setupMockPersistence()
-
-        const mockSecurityResult = {
-            "success": true,
-            "error": {
-                "code": "Test Code",
-                "message": "Test Message"
-            },
-            "user": {
-                "payload": {
-                    "sub": "User"
-                }
-            }
-        }
-
-        var context = {   
-            res: {},
-            log: function (msg) {}        
-        }
-
-        const request = {}
-
-        const mockList = [
-            {
-                "name": "Unit 1"
-            },
-            {
-                "name": "Unit 2"
-            }
-        ]
-
-        const expectations = [
-            mockSecurity
-                .expects("checkToken")
-                .once()
-                .withArgs(context, request)
-                .returns(mockSecurityResult),
-            mockPersistence.expects("getEntriesOfKind")
-                .once()
-                .withExactArgs('unit', ['name'])
-                .returns(mockList),
-        ]
-            
-        await uut(context, request);
-
-        expect(context.res.body).toEqual(mockList);
-
-        expectations.map(e => e.verify())
-
-        mockSecurity.restore()
-        mockPersistence.restore()
+        await runSuccessfulListTest(true)
     });
 
     test('should correctly list units even if authentication fails.', async function () {
-        const mockSecurity = setupMockSecurity()
-        const mockPersistence = setupMockPersistence()
-
-        const mockSecurityResult = {
-            "success": false,
-            "error": {
-                "code": "Test Code",
-                "message": "Test Message"
-            }
-        }
-
-        var context = {   
-            res: {},
-            log: function (msg) {}        
-        }
-
-        const request = {}
-
-        const mockList = [
-            {
-                "name": "Unit 1"
-            },
-            {
-                "name": "Unit 2"
-            }
-        ]
-
-        const expectations = [
-            mockSecurity
-                .expects("checkToken")
-                .once()
-                .withArgs(context, request)
-                .returns(mockSecurityResult),
-            mockPersistence.expects("getEntriesOfKind")
-                .once()
-                .withExactArgs('unit', ['name'])
-                .returns(mockList),
-        ]
-            
-        await uut(context, request);
-
-        expect(context.res.body).toEqual(mockList);
-
-        expectations.map(e => e.verify())
-
-        mockSecurity.restore()
-        mockPersistence.restore()
+        await runSuccessfulListTest(false)
     });
 });
