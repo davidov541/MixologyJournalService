de346b51f662d73d611ba1cbd1df611f4baaad60
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-03 15:48:19 -0500

    Adding ingredients as someone who isn't the admin is forbidden.

--- test/addingredient/addingredient.test.js
+++ test/addingredient/addingredient.test.js
@@ -49,6 +49,61 @@ describe('Add Ingredient Function Tests', function () {
         mockSecurity.restore()
     });
 
+    test('should fail if a user that is not an administrator tries to add an ingredient.', async function () {
+        const mockSecurity = setupMockSecurity()
+        const mockPersistence = setupMockPersistence()
+
+        const mockSecurityResult = {
+            "success": true,
+            "error": {
+                "code": "Test Code",
+                "message": "Test Message"
+            },
+            "user": {
+                "payload": {
+                    "sub": "User"
+                }
+            }
+        }
+
+        var context = {   
+            res: {},
+            log: function (msg) {}        
+        }
+
+        const request = {
+            "body": {
+                "name": "Test Ingredient"
+            }
+        }
+
+        const expectations = [
+            mockSecurity
+                .expects("checkToken")
+                .once()
+                .withArgs(context, request)
+                .returns(mockSecurityResult),
+            mockSecurity
+                .expects("isAdmin")
+                .once()
+                .withArgs(mockSecurityResult.user)
+                .returns(false)
+        ]
+            
+        await uut(context, request);
+
+        const expectedResponse = {
+            "status": 401,
+            "body": "User cannot add ingredients."
+        }
+        expect(context.res).toEqual(expectedResponse);
+
+        expectations.map(e => e.verify())
+
+        mockPersistence.restore()
+        mockSecurity.restore()
+    });
+
     test('should correctly add the ingredient if authentication succeeds.', async function () {
         const mockSecurity = setupMockSecurity()
         const mockPersistence = setupMockPersistence()
@@ -83,6 +138,11 @@ describe('Add Ingredient Function Tests', function () {
                 .once()
                 .withArgs(context, request)
                 .returns(mockSecurityResult),
+            mockSecurity
+                .expects("isAdmin")
+                .once()
+                .withArgs(mockSecurityResult.user)
+                .returns(true),
             mockPersistence.expects("createEntryOfKind")
                 .once()
                 .withExactArgs('ingredient', sinon.match.any, {
